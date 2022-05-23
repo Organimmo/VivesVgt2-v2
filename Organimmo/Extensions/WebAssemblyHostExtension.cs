9e028dc830391dbcb82ba80cc0790aae60b709f6
DeWintereArne <arne.dewintere@student.vives.be>
2022-05-23 11:54:53 +0200

    added translation to ui

---
+++ Organimmo/Extensions/WebAssemblyHostExtension.cs
@@ -0,0 +1,32 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System.Globalization;
+using System.Threading.Tasks;
+
+namespace Organimmo.UI.Blazor.Extensions
+{
+    public static class WebAssemblyHostExtension
+    {
+        public async static Task SetDefaultCulture(this WebAssemblyHost host)
+        {
+            var localstorage = host.Services.GetRequiredService<ILocalStorageService>();
+            var cultureFromLS = await localstorage.GetItemAsync<string>("culture");
+
+            CultureInfo culture;
+
+            if (cultureFromLS != null)
+            {
+                // remembers what language was used last time before exit application
+                culture = new CultureInfo(cultureFromLS);
+            }
+            else
+            {
+                culture = new CultureInfo("nl-NL");
+            }
+
+            CultureInfo.DefaultThreadCurrentCulture = culture;
+            CultureInfo.DefaultThreadCurrentUICulture = culture;
+        }
+    }
+}
--- Organimmo/Program.cs
+++ Organimmo/Program.cs
@@ -1,15 +1,32 @@
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 using Organimmo;
 using Organimmo.Services;
 using Organimmo.UI.Blazor;
+using Organimmo.UI.Blazor.Extensions;
+namespace Organimmo.UI.Blazor
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
+            builder.Services.AddLocalization();
+            builder.Services.AddBlazoredLocalStorage();
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddMudServices();
+            var host = builder.Build();
+            await host.SetDefaultCulture();
+            await host.RunAsync();
+        }
+    }
+}
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddMudServices();
-await builder.Build().RunAsync();
+
+
 
